f9ffbda062eb753084b5b4c30414d97adbb6abcc
cxrmine <amaryllismain09@gmail.com>
2025-06-01 15:59:41 +0200

    feat: handle all cases for ex00

--- megaphone.cpp
+++ megaphone.cpp
@@ -10,17 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
 #include <iostream>
 #include <ostream>
-#include <string>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   if (argc == 1) {
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     return 0;
   }
 
-  std::string s = argv[1];
+  for (std::size_t i = 1; argv[i]; i++) {
+	  for (std::size_t j = 0; argv[i][j]; j++) {
+    		if (argv[i][j] >= 97 && argv[i][j] <= 122)
+      			argv[i][j] -= 32;
+	  }
+  	std::cout << argv[i];
+  }
+
+  std::cout << std::endl;
   return 0;
 }
