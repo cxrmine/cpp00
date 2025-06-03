b808b4b40fec07cd746f0f06b9ab450db0ba5882
cxrmine <amaryllismain09@gmail.com>
2025-06-03 15:46:31 +0200

    feat: add the posibility to replace last contact if phonebook full

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -27,8 +27,6 @@ bool PhoneBook::isFull() {
 void PhoneBook::add(Contact *contact, PhoneBook *phoneBook) {
   std::string input;
 
-  if (PhoneBook::isFull())
-    return;
   std::cout << "first name: ";
   std::cin >> input;
   contact->firstName = input;
@@ -44,19 +42,25 @@ void PhoneBook::add(Contact *contact, PhoneBook *phoneBook) {
   std::cout << "darkest secret: ";
   std::cin >> input;
   contact->darkestSecret = input;
+  if (PhoneBook::isFull())
+  {
+	contact->index = 8;
+	phoneBook->contacts[7] = *contact;
+    return;
+  }
   contact->index = phoneBook->size + 1;
   phoneBook->contacts[phoneBook->size] = *contact;
   phoneBook->size++;
 }
 
-void PhoneBook::print(PhoneBook *phoneBook) {
+void PhoneBook::print(PhoneBook *phoneBook, std::size_t index) {
   std::string string_index = "index";
   std::string string_fname = "first name";
   std::string string_lname = "last name";
   std::string string_nickname = "nickname";
   if (phoneBook->size == 0)
     return;
-  std::cout << std::setfill(' ') << std::setw(10 - string_index.size())
+  std::cout << std::setfill(' ') << std::setw(string_index.size())
             << string_index << "|";
   std::cout << std::setfill(' ') << std::setw(string_fname.size())
             << string_fname << "|";
@@ -64,37 +68,41 @@ void PhoneBook::print(PhoneBook *phoneBook) {
             << string_lname << "|";
   std::cout << std::setfill(' ') << std::setw(string_nickname.size())
             << string_nickname << "\n";
-  for (std::size_t i = 0; i < phoneBook->size; i++) {
-    std::cout << std::setfill(' ') << std::setw(10 - string_index.size())
-              << phoneBook->contacts[i].index << "|";
-    std::cout << std::setfill(' ') << std::setw(string_fname.size())
-              << phoneBook->contacts[i].firstName << "|";
-    std::cout << std::setfill(' ') << std::setw(string_lname.size())
-              << phoneBook->contacts[i].lastName << "|";
-    std::cout << std::setfill(' ') << std::setw(string_nickname.size())
-              << phoneBook->contacts[i].nickname << "\n";
-  }
+  std::cout << std::setfill(' ') << std::setw(string_index.size())
+            << phoneBook->contacts[index].index << "|";
+  std::cout << std::setfill(' ') << std::setw(string_fname.size())
+            << phoneBook->contacts[index].firstName << "|";
+  std::cout << std::setfill(' ') << std::setw(string_lname.size())
+            << phoneBook->contacts[index].lastName << "|";
+  std::cout << std::setfill(' ') << std::setw(string_nickname.size())
+            << phoneBook->contacts[index].nickname << "\n";
   return;
 }
 
-void PhoneBook::search(Contact *contact, PhoneBook *phoneBook) {
+void PhoneBook::search(PhoneBook *phoneBook) {
   std::stringstream ss;
   std::size_t index;
   std::string input;
 
-  (void)contact;
   std::cout << "Enter index: ";
   std::cin >> input;
   ss << input;
   ss >> index;
+
+  if (index > phoneBook->size)
+  {
+	std::cout << "this contact doesn't exists..." << "\n";
+	return ;
+  }
+
   if (phoneBook->size == 0)
-    return;
+  {
+	std::cout << "you haven't added any contacts yet, please do with ADD" << "\n";
+    return ;
+  }
+
   index = index - 1;
-  std::cout << phoneBook->contacts[index].index << "\n";
-  std::cout << phoneBook->contacts[index].firstName << "\n";
-  std::cout << phoneBook->contacts[index].lastName << "\n";
-  std::cout << phoneBook->contacts[index].darkestSecret << "\n";
-  std::cout << phoneBook->contacts[index].phoneNumber << "\n";
+  PhoneBook::print(phoneBook, index);
   return;
 }
 
