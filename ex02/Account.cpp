69b142722305eb2949fce1a831cb6348298068c1
cxrmine <amaryllismain09@gmail.com>
2025-06-04 14:16:55 +0200

    chore: write the boilerplate code

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -12,6 +12,42 @@
 
 #include "Account.hpp"
 
-Account::Account() {}
+Account::Account(int initial_deposit) {}
 
-Account::~Account() {}
+Account::~Account(void) {}
+
+int Account::getNbAccounts(void) {
+	return 0;
+}
+
+int Account::getTotalAmount(void) {
+	return 0;
+}
+
+int Account::getNbDeposits(void) {
+	return 0;
+}
+
+int Account::getNbWithdrawals(void) {
+	return 0;
+}
+
+void Account::displayAccountsInfos(void) {
+	return ;
+}
+
+void Account::makeDeposit(int deposit) {
+	return ;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+	return false;
+}
+
+int Account::checkAmount(void) const {
+	return 0;
+}
+
+void Account::displayStatus(void) const {
+	return ;
+}
